1c29b7d2e48a88bd7ca8f328bf0c7ee07a394044
ADRIANHHALIM <adrianhalim05@gmail.com>
2025-05-20 21:51:54 +0700

    Debugging Image Display Issue in Job Details Section

--- Anotasi_Image/master/urls.py
+++ Anotasi_Image/master/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from . import views
-from .views import activate
 
 app_name = 'master'
 
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
-    path("activate/<uidb64>/<token>/", activate, name="activate"),  
+    path("activate/<uidb64>/<token>/", views.activate, name="activate"),  
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path("home/", views.home_view, name="home"),
